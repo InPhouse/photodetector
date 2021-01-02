8c23c4fbf7e04bcd1f510020e18f21318b324578
prakash thapa <pt6769759@gmail.com>
2021-01-02 14:40:22 +0545

    first commit

--- app.py
+++ app.py
@@ -1,10 +1,25 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for
+from imageai.Prediction import ImagePrediction
+import os
 
 app = Flask(__name__)
 
 @app.route('/', methods=['POST', 'GET'])
 def index():
     if request.method == 'POST':
-        return 'Success.'
+      fname = request.form['file'];
+      execution_path = os.getcwd()
+
+      prediction = ImagePrediction()
+      prediction.setModelTypeAsResNet()
+      prediction.setModelPath(os.path.join(execution_path, "static/resnet50_weights_tf_dim_ordering_tf_kernels.h5"))
+      prediction.loadModel()
+
+      predictions, probabilities = prediction.predictImage(fname, result_count=10 )
+
+      return render_template('detect.html', predict=predictions, eachProb=probabilities)
     
     return render_template('index.html')
+
+
+
